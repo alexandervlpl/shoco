6c6c9d01932ed7e76886b89bad1856d285e5202c
apl <alexander.vlpl@gmail.com>
2020-05-07 21:19:43 +0300

    improving node_test.js

--- node_test.js
+++ node_test.js
@@ -1,3 +1,10 @@
 const shoco = require("./shoco");
-var result = shoco.decompress(shoco.compress("hello world"));
+
+const util = require("util");
+let encoder = new util.TextEncoder();
+
+var input = process.argv[2];
+var comp = shoco.compress(process.argv[2]);
+var result = shoco.decompress(comp);
+console.log(encoder.encode(input).length + " > " + comp.length);
 console.log(result);
